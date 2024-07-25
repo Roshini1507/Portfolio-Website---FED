d4aaaccee2e18d9d4516e04d1046178838fe9925
Roshini1507 <149648456+Roshini1507@users.noreply.github.com>
2024-07-25 17:31:28 +0530

    Add files via upload

---
+++ Portfolio project-FED/portfolio.js
@@ -0,0 +1,8 @@
+var typingEffect = new Typed(".typedText",{
+    strings: ["CS Undergrad", "AI-ML Enthusiast", "Data Analyst" , "Web Developer", "Coder"],
+
+    loop: true,
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+})
